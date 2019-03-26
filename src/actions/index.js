6fa263e97888e5ebcfe048664cb001acaf2a5a91
Todd Jacobus <rtjacobus1@gmail.com>
2019-03-26 06:37:41 -0600

    refactored readability to only parse html; axios to make actual http request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import gdeltDaily from "../apis/gdeltDaily";
 import getDetails from "../apis/articleParser"
+import read from 'readability-js';
 
 export const fetchArticles = (search="") => async dispatch => {
 
@@ -12,8 +13,38 @@ export const fetchArticles = (search="") => async dispatch => {
   dispatch({type:'GET_ARTICLES', payload: response.data.features})
 };
 
+// NOTE: The API returns a response, and readability parses the article just fine.
+// However, an empty object is sent along to the Redux store before the response
+// is returned from axios...  something to do with the 'await' syntax?
 
 export const loadArticleDetails = (url=null) => async dispatch => {
-  const articleDetails = await getDetails(url)
-  dispatch({type:'GET_DETAILS', payload: articleDetails})
+  const response = await getDetails.get(url);
+  dispatch({type:'GET_DETAILS',
+    payload:  read(response.data, (err, article, meta) => {
+                console.log(article)
+                return article
+              })
+  })
 }
+
+
+
+// console.log('Fetching article details...')
+// if (url) {
+//   // TODO: fix error with api, or find a new api that works more easily...
+//   // Keeps passing undefined into callback.
+//   read(url, (err, article, meta) => {
+//     console.log(article)
+//     console.log(meta)
+//     console.log(err)
+//   });
+// } else {
+//   console.log("Still loading...")
+//   return {
+//     'title': 'loading...',
+//     'content': 'loading...',
+//     'source': 'loading...',
+//     'dom': 'loading...',
+//     'responseObj': 'loading...'
+//   }
+// }
--- src/apis/articleParser.js
+++ src/apis/articleParser.js
@@ -1,50 +1,5 @@
-import read from 'readability-js';
+import axios from 'axios';
 
-export default (url) => {
-  console.log('Fetching article details...')
-  if (url) {
-    // TODO: fix error with api, or find a new api that works more easily...
-    // Keeps passing undefined into callback.
-    read(url, (err, article, meta) => {
-      console.log(article)
-      console.log(meta)
-      console.log(err)
-    });
-  } else {
-    console.log("Still loading...")
-    return {
-      'title': 'loading...',
-      'content': 'loading...',
-      'source': 'loading...',
-      'dom': 'loading...',
-      'responseObj': 'loading...'
-    }
-  }
-
-}
-
-
-
-// try {
-//   const contents = article
-//   const contents = {
-//     'title': article.title,
-//     'content': article.content,
-//     'source': article.html,
-//     'dom': article.document,
-//     'responseObj': article.meta
-//   }
-//   article.close();
-//   console.log("Hurray!")
-//   return contents
-// }
-// catch (err) {
-//   console.log("woopsy... " + err)
-//   return {
-//     'title': 'Could not fetch article detail...',
-//     'content': '',
-//     'source': '',
-//     'dom': '',
-//     'responseObj': ''
-//   }
-// }
+export default axios.create({
+  baseURL:''
+})
